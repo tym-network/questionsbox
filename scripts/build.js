9fc7cc3a72b80c5535a18029a7c9037bce769d47
Théotime Loiseau <theotime.loiseau@gmail.com>
2018-04-17 23:16:07 +0200

    New build process

--- scripts/build.js
+++ scripts/build.js
@@ -1,40 +1,65 @@
 const webpack = require('webpack');
-const NwBuilder = require('nw-builder');
+const builder = require("electron-builder");
+const path = require('path');
 
-const webpackConfiguration = require('./../webpack.prod.js');
+const webpackRendererConfiguration = require('./../webpack.prod.js');
+const webpackMainConfiguration = require('./../webpack.main.js');
 
-const nw = new NwBuilder({
-    files: ['./web/**/**', './package.json', './questions.json'], // use the glob format
-    platforms: ['osx64', 'win', 'linux'],
-    flavor: 'normal',
-    appName: 'QuestionsBox',
-    buildDir: './build',
-    cacheDir: './cache',
-    macIcns: './src/assets/img/logo.icns'
-});
-
-nw.on('log', console.log);
-
-
-const compiler = webpack(webpackConfiguration);
+const rendererCompiler = webpack(webpackRendererConfiguration);
+const mainCompiler = webpack(webpackMainConfiguration);
 
 // Build the source code
-compiler.run((err, stats) => {
-    if (err || stats.hasErrors()) {
-        console.log('Error while building source code (Webpack)');
-        console.error(err, stats);
-    } else {
-        // Start building the code into a NW app
-        console.log('Source code build (Webpack)!');
-        buildNW();
-    }
+const rendererCompilePromise = new Promise((res, rej) => {
+    rendererCompiler.run((err, stats) => {
+        if (err || stats.hasErrors()) {
+            console.log('Error while building source code (Webpack)');
+            console.error(err, stats);
+            rej(err);
+        } else {
+            // Start building the code into a NW app
+            console.log('Source code built (Webpack)!');
+            res();
+        }
+    });
 });
 
-function buildNW() {
-    nw.build().then(function () {
-        console.log('Build complete');
-    }).catch(function (error) {
-        console.log('Error while building');
-        console.error(error);
+const mainCompilePromise = new Promise((res, rej) => {
+    mainCompiler.run((err, stats) => {
+        if (err || stats.hasErrors()) {
+            console.log('Error while building source code (Webpack)');
+            console.error(err, stats);
+            rej(err);
+        } else {
+            // Start building the code into a NW app
+            console.log('Source code built (Webpack)!');
+            res();
+        }
     });
+});
+
+Promise.all([rendererCompilePromise, mainCompilePromise]).then(() => {
+    buildApp();
+});
+
+
+// Build the app
+function buildApp() {
+    builder.build({
+        config: {
+            appId: 'com.questionsbox',
+            productName: 'QuestionsBox',
+            directories: {
+                output: 'build'
+            },
+            mac: {
+                target: 'dmg',
+                icon: path.join(__dirname, '..', 'src', 'assets', 'img', 'logo.icns')
+            }
+        }
+    }).then(() => {
+        console.log('App built!');
+    }).catch((error) => {
+        console.log('Error while building app (electron-builder)');
+        console.error(error);
+    })
 }
