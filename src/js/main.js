44f5c7b4fe2827cdd5e280b34c702eb2b028b6bb
Théotime Loiseau <theotime.loiseau@gmail.com>
2019-06-23 19:47:18 +0200

    Continue to update dependencies

--- src/js/main.js
+++ src/js/main.js
@@ -24,7 +24,13 @@ const url = require('url');
 let win;
 
 function createWindow() {
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
     win.loadURL(url.format({
         pathname: path.join(__dirname, '..', 'html', 'index.html'),
