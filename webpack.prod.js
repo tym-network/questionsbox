d1115cc37d1cff19b0d142be10fffc96fea54ffc
Théotime Loiseau <theotime.loiseau@gmail.com>
2019-07-07 17:27:58 +0200

    Remove clean webpack plugin and handle clean on our own

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,7 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const merge = require('webpack-merge');
 
 const common = require('./webpack.common.js');
 
-const pathsToClean = [
-    'web'
-]
-
 module.exports = merge(common, {
-    mode: 'production',
-    plugins: [
-        new CleanWebpackPlugin(pathsToClean)
-    ]
+    mode: 'production'
 });
